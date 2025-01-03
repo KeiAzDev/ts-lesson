7e722c98774d2cb38f10c8d7c7f076bc63a628f0
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-03 14:12:26 -0800

    型推論について

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-import axions from 'axios';
+// import axions from 'axios';
 
 
-let hello: string = 'hello';
-console.log(hello);
+// let hello: string = 'hello';
+// console.log(hello);
 
-function add(a: number, b: number): number {
-  return a + b;
-}
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+let hasValue: boolean = true;
+let count: number = 12;
+let float = 3.12;
+let negative = -0.23;
+let single: string = 'aa';
+//冗長になるので基本的にこのタイプをつけずに型推論で書くことが望ましい

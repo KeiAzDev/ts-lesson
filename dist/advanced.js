8f7f9131396da01259f45d5689f8ee4b45e7869f
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-05 14:27:35 -0800

    ＆を用いてインターセクション型を定義

---
+++ dist/advanced.js
@@ -0,0 +1,7 @@
+"use strict";
+//interface EngineerBlogger extends Engineer, Blogger { }
+const quill = {
+    name: 'Quill',
+    role: 'front-end',
+    follower: 1000,
+};
---
+++ src/advanced.ts
@@ -0,0 +1,20 @@
+//interfaceでも問題なし
+type Engineer = {
+  name: string;
+  role: string;
+}
+type Blogger = {
+  name: string;
+  follower: number;
+}
+type EngineerBlogger = Engineer & Blogger;
+//interface EngineerBlogger extends Engineer, Blogger { }
+const quill: EngineerBlogger = {
+  name: 'Quill',
+  role: 'front-end',
+  follower: 1000,
+}
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber;

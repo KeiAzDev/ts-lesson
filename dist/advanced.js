7bc1f965d89900eb5af3faa7b55c273081bcfa58
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-05 14:48:07 -0800

    条件文を使って型を絞り込む３つのtype guard

--- dist/advanced.js
+++ dist/advanced.js
@@ -5,3 +5,40 @@ const quill = {
     role: 'front-end',
     follower: 1000,
 };
+function toUpperCase(x) {
+    if (typeof x === 'string') {
+        return x.toUpperCase();
+    }
+    else {
+        return '';
+    }
+}
+function describeProfile(nomadWorker) {
+    console.log(nomadWorker.name);
+    if ('role' in nomadWorker) {
+        console.log(nomadWorker.role);
+    }
+    if ('follower' in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+class Dog {
+    speak() {
+        console.log('bow-wow');
+    }
+}
+class Bird {
+    speak() {
+        console.log('tweet-tweet');
+    }
+    fly() {
+        console.log('flutter');
+    }
+}
+function havePet(pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+havePet(new Bird());

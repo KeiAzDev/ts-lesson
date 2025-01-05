311c7f3e48714d8bca5e8d8d6fc20a1307318c9c
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-05 10:48:52 -0800

    implements,interface,class

--- dist/interface.js
+++ dist/interface.js
@@ -1,8 +1,27 @@
 "use strict";
-const human = {
+class Developer {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+    greeting(message) {
+        console.log(message);
+    }
+}
+const tmpDeveloper = {
     name: 'Quill',
     age: 38,
+    experience: 3,
     greeting(message) {
         console.log(message);
     }
 };
+const user = tmpDeveloper;
+// const human = {
+//   name: 'Quill',
+//   age: 38,
+//   greeting(message: string) {
+//     console.log(message);
+//   }
+// }
--- src/interface.ts
+++ src/interface.ts
@@ -3,11 +3,29 @@ interface Human {
   age: number;
   greeting(message: string): void;
 }
+class Developer implements Human {
+  constructor(public name: string, public age: number, public experience: number) {
+  }
 
-const human = {
+  greeting(message: string): void {
+    console.log(message);
+  }
+}
+const tmpDeveloper = {
   name: 'Quill',
   age: 38,
+  experience: 3,
   greeting(message: string) {
     console.log(message);
   }
 }
+const user: Human = tmpDeveloper;
+
+
+// const human = {
+//   name: 'Quill',
+//   age: 38,
+//   greeting(message: string) {
+//     console.log(message);
+//   }
+// }

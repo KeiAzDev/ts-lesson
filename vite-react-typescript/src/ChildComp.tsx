1efe2404ba4c7c9dac6ef409b603f657224bb08e
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-08 17:18:10 -0800

    hooksについて

--- vite-react-typescript/src/ChildComp.tsx
+++ vite-react-typescript/src/ChildComp.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState, MouseEvent } from "react";
 
 interface ChildCompProps {
   message: string;
@@ -6,11 +6,17 @@ interface ChildCompProps {
 }
 
 function ChildComp({message, children}: ChildCompProps) {
+  const [count, setCount] = useState<number | string>(0);
+  function updateCount(event: MouseEvent) {
+    setCount(event.clientY);
+  }
   return(
     <div>
       <h2>ChildComp</h2>
       <p>{message}</p>
       <div>{children}</div>
+      <p>count: {count}</p>
+      <button onClick={updateCount}>+1</button>
     </div>
   )
 }
